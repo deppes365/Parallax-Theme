ef48a84d39bdcdc92bc45dc02883b4131fc18cd4
Darren Eppes <deppes365@gmail.com>
2022-08-07 17:07:39 -0400

    parallax effect and mobile menu added

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict'
 
 // Variables
-const menuBtn = document.querySelector('.mobile-menu')
+const menuBtn = document.querySelector('.mobile-menu-btn')
+const navLinksContainer = document.querySelector('.nav-links')
+const navLinks = document.querySelectorAll('.nav-link')
 
 // Event Listeners
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active')
+    menuBtn.classList.toggle('active');
+    navLinksContainer.classList.toggle('active')
+})
+
+navLinksContainer.addEventListener('click', (e) => {
+    const clickedElement = e.target.classList;
+
+    if(clickedElement.contains('active')) {
+        return
+    } else if(!clickedElement.contains('active')) {
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+        })
+
+        e.target.classList.add('active')
+    }
 })
